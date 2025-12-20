6f5d13ff41814ba8bff3a1d2ce093fd3e101bae4
nevinnelsonn-ctrl <nevinnelsonn@gmail.com>
2025-12-20 13:24:27 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const connectDB = require('./config/db');
+
+// Connect to MongoDB
+connectDB();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Test route
+app.get('/', (req, res) => {
+  res.json({ message: 'Server is running successfully!' });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
